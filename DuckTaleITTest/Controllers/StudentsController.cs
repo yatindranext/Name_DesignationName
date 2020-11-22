1605aba8c2ca5e287a96c271fa6d04ea11718ddf
yatindranext <yatindranext@gmail.com>
2020-11-22 22:26:26 +0530

    first commit

---
+++ DuckTaleITTest/Controllers/StudentsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DuckTaleITTest.Models;
+
+namespace DuckTaleITTest.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private DB db = new DB();
+
+        // GET: Students
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //return View(db.Students.ToList());
+
+            return View(db.Students.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string CatSearch, string NameSearch)
+        {
+            //return View(db.Students.ToList());
+
+            if(CatSearch == "Class" && NameSearch != null)
+            {
+                return View(db.Students.Where(x => x.Std_Class.Contains(NameSearch)));
+            }
+            else if (CatSearch == "Subject" && NameSearch != null)
+            {
+                return View(db.Students.Where(x => x.Subjects.Select(y => y.Name).Contains(NameSearch)));
+            }
+            else
+            {
+                return View(db.Students.Where(x => x.Std_FirstName.Contains(NameSearch) || x.Std_LastName.Contains(NameSearch) || NameSearch == null).ToList());
+            }
+            
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View(new Student()
+            {
+                Std_Subjects = new List<Subject>()
+                {
+                    new Subject()
+                }
+            });
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Std_Id,Std_FirstName,Std_LastName,Std_Class,Subjects,Marks")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return Json(true);
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Std_Id,Std_FirstName,Std_LastName,Std_Class,Subject,Marks")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DuckTaleITTest/Models/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace DuckTaleITTest.Models
+{
+    [Table("tbl_student")]
+    public class Student
+    {
+        [Key]
+        public int Std_Id { get; set; }
+
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$",ErrorMessage ="Invalid")]
+        [Required(ErrorMessage = "FirstName is required")]
+        public string Std_FirstName { get; set; }
+
+        [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$",ErrorMessage = "Invalid")]
+        [Required(ErrorMessage = "LastName is required")]
+        public string Std_LastName { get; set; }
+
+
+        [Range(1, 12, ErrorMessage = "Marks should Between 0 To 12")]
+        [Required(ErrorMessage = "Class is required")]
+        public string Std_Class { get; set; }
+
+        public List<Subject> Std_Subjects { get; set; }
+    }
+
+    [Table("tbl_subject")]
+    public class Subject
+    {
+        [Key]
+        public int Sub_Id { get; set; }
+
+        [ForeignKey("studentPk_subject")]
+        public int Std_Id { get; set; }
+
+        public string Sub_Name { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Marks should Between 0 To 100")]
+        [Required(ErrorMessage = "Marks is required")]
+        public int Sub_Marks { get; set; }
+
+    }
+
+
+    public class DB:DbContext
+    {
+        public DB():base("cs")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> subjects { get; set; }
+    }
+
+}
